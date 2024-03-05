42642a5e2dfd5347c4a260526b4ab49783fe61d3
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-03-05 22:46:35 +0530

    adding tables to admin

--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,114 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Institution(models.Model):
+    TYPE_CHOICES = [
+        ("SCHOOL", "school"),
+        ("COLLEGE", "college"),
+    ]
+
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    instituion_type = models.CharField(max_length=7, choices=TYPE_CHOICES)
+    education_system = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "institution"
+
+
+class Community(models.Model):
+    LEVEL_CHOICES = [
+        ("GLOBAL", "global"),
+        ("NATIONAL", "national"),
+        ("REGIONAL", "regional"),
+        ("LOCAL", "local"),
+    ]
+    TYPE_CHOICES = [
+        ("TECHNICAL", "technical"),
+        ("NON-TECHNICAL", "non-technical"),
+        ("SERVICE", "service"),
+    ]
+
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+    community_type = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "community"
+
+
+class Department(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "department"
+
+
+class Faculty(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    faculty_id = models.CharField(max_length=100)
+    user = models.OneToOneField("users.User", on_delete=models.CASCADE)
+    institution = models.ForeignKey("Institution", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "faculty"
+
+
+class Student(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    roll_number = models.CharField(max_length=100)
+    class_or_semester = models.IntegerField()
+    user = models.OneToOneField("users.User", on_delete=models.CASCADE)
+    institution = models.ForeignKey("Institution", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "student"
+
+
+class FacultyDepartmentLink(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    faculty = models.ForeignKey("Faculty", on_delete=models.CASCADE)
+    department = models.ForeignKey("Department", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "faculty_department_link"
+
+
+class StudentDepartmentLink(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    student = models.ForeignKey("Student", on_delete=models.CASCADE)
+    department = models.ForeignKey("Department", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "student_department_link"
+
+
+class Course(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    handling_faculty = models.ManyToManyField("Faculty")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "course"
--- admin/views.py
+++ admin/views.py
@@ -1,29 +1,37 @@
 from ninja import Router
+from typing import Any
 
 from utils.authentication import role_required
+from admin.schemas import GiveRolesSchema
+from users.models import User, Role
 
 router = Router()
 
 
-@router.post("/institution/")
+@router.post("/institution/", response={200: Any, 403: Any})
 @role_required(["Admin"])
-def give_institution_role(request):
+def give_institution_role(request, data: GiveRolesSchema):
+    users = User.object.filter(id__in=data.user_ids)
     return {"message": "Institution role given"}
+
 
 @router.post("/community/")
 @role_required(["Admin"])
 def give_community_role(request):
     return {"message": "Community role given"}
 
+
 @router.post("/faculty/")
 @role_required(["Institution"])
 def give_faculty_role(request):
     return {"message": "Faculty role given"}
 
+
 @router.post("/student/")
 @role_required(["Institution"])
 def give_student_role(request):
     return {"message": "Student role given"}
+
 
 @router.post("/community-member/")
 @role_required(["Community"])
