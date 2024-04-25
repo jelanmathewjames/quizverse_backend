c0d0cd3b4794abbba8439d91cee6d8a2a9462e6f
jelanmathewjames <72068016+jelanmathewjames@users.noreply.github.com>
2024-04-25 23:28:13 +0530

    Update settings.py

--- quizverse_backend/settings.py
+++ quizverse_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ config("WEBSITE_HOSTNAME") ] if config("WEBSITE_HOSTNAME", None) else []
 
 
 # Application definition
