1c858128bca75524d3135fc6c9cfd7cfbf3b4a04
jelanmathewjames <72068016+jelanmathewjames@users.noreply.github.com>
2024-05-01 12:45:49 +0530

    Update settings.py

--- quizverse_backend/settings.py
+++ quizverse_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = True if os.environ.get("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = [ os.environ.get("WEBSITE_HOSTNAME") ] if os.environ.get("WEBSITE_HOSTNAME", None) else []
 
