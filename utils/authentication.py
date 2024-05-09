be340ec34d61702ef5a56e691001f11ab18ac85a
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-05-09 10:26:13 +0530

    fix role required decorator

--- utils/authentication.py
+++ utils/authentication.py
@@ -106,10 +106,13 @@ def role_required(roles):
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(request, *args, **kwargs):
-            if getattr(request, "auth", {}).get("role") in roles:
-                return view_func(request, *args, **kwargs)
-            else:
+
+            if not (given_roles := getattr(request, "auth", {}).get("roles")):
                 raise InSufficientPermission
+            for role in given_roles:
+                if role in roles:
+                    return view_func(request, *args, **kwargs)
+            raise InSufficientPermission
 
         return wrapper
 
